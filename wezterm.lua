add698ef25d2f6e9821200fa57b116dd90c2fd46
HDA <hieronimdaniel@gmail.com>
2024-08-27 13:49:54 +0200

    change tab bar

--- wezterm.lua
+++ wezterm.lua
@@ -133,27 +133,12 @@ config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = false
 config.show_new_tab_button_in_tab_bar = false
 
--- tmux status
-wezterm.on("update-right-status", function(window, _)
-	local SOLID_LEFT_ARROW = ""
-	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-	local prefix = ""
-
-	if window:leader_is_active() then
-		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-	end
-
-	if window:active_tab():tab_id() ~= 0 then
-		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-	end -- arrow color based on if tab is first pane
-
-	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#b7bdf8" } },
-		{ Text = prefix },
-		ARROW_FOREGROUND,
-		{ Text = SOLID_LEFT_ARROW },
-	}))
-end)
-
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config, {
+	enabled_modules = {
+		username = false,
+		hostname = true,
+		clock = false,
+	},
+})
 return config
