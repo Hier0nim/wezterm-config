acf0821619eb1478843fc8bf534402111c55b467
HDA <hieronimdaniel@gmail.com>
2024-08-29 11:44:27 +0200

    change size of displayed window

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -9,6 +9,7 @@ M.apply_to_config = function(c)
 		split = scheme.ansi[2],
 	}
 	c.inactive_pane_hsb = { brightness = 0.9 }
+	c.window_padding = { left = "1cell", right = "1cell", top = "0.5cell", bottom = 0 }
 	c.window_decorations = "RESIZE"
 	c.show_new_tab_button_in_tab_bar = false
 end
